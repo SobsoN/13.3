34c0b0d3601d298a9e800564279e5d7af8320792
Grzegorz Sobczyk <grzegorzsobczykdev@gmail.com>
2018-04-05 23:34:06 +0200

    Fin

---
+++ program.js
@@ -0,0 +1,30 @@
+process.stdin.setEncoding('utf-8');
+process.stdin.on('readable', function() {
+    // metoda .read() ma za zadanie odczytać co użytkownik podał na wejściu
+    var input = process.stdin.read();
+    process.stdout.write('Write "/version" or "/lang" or "/exit" \n');
+
+    if (input !== null) {
+    	var instruction = input.toString().trim();
+    	switch(instruction) {
+    		case '/exit' :
+    			process.stdout.write('Quitting app!\n');
+            	process.exit();
+    			break;
+
+    		case '/version' :
+    			console.log(process.versions);
+    			break;
+
+    		case '/lang' :
+    			console.log(process.env);
+    			break;
+
+    		default :
+    			process.stderr.write('Wrong command\n');
+    			break;
+    	}
+    }
+
+
+});
