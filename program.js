3503a2c0d3e18c56f077bb0c8a821ce10979b14f
Grzegorz Sobczyk <grzegorzsobczykdev@gmail.com>
2018-04-05 23:35:00 +0200

    Fin

--- program.js
+++ program.js
@@ -1,6 +1,5 @@
 process.stdin.setEncoding('utf-8');
 process.stdin.on('readable', function() {
-    // metoda .read() ma za zadanie odczytać co użytkownik podał na wejściu
     var input = process.stdin.read();
     process.stdout.write('Write "/version" or "/lang" or "/exit" \n');
 
@@ -25,6 +24,4 @@ process.stdin.on('readable', function() {
     			break;
     	}
     }
-
-
 });
